d1db8691715b5b566cd514134dd84b34d8b3e026
nishionemui <anlihvosion227@gmail.com>
2020-03-12 12:25:06 +0900

    メッセージ

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'groups#index'
  resources :users, only: [:edit, :update]
- resources :groups, only: [:index, :new, :create, :edit, :update]
+ resources :groups, only: [:index, :new, :create, :edit, :update, :edit] do
+   resources :messages, only: [:index, :create, :edit]
+  end
 end
